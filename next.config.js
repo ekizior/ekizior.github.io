2cf10eb3d91cd2511471376d0e676fcbf5bde31f
Erik Kizior <ekizior@comcast.net>
2025-07-10 00:06:26 -0400

    Fix Next.js config: remove basePath for GitHub Pages root domain

--- next.config.js
+++ next.config.js
@@ -5,8 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/ekizior.github.io' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/ekizior.github.io' : '',
 }
 
 module.exports = nextConfig 
